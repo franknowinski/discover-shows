5b38dd317c51e6fedc69ddb85b188fae8456dd94
frankNowinski <nowinski.frank@gmail.com>
2016-04-05 23:23:43 -0800

    revised user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,6 @@
 class UserController < ApplicationController
 
   def spotify
-    # User.from_omniauth(RSpotify::User.new(request.env['omniauth.auth']))
     User.from_omniauth(request.env['omniauth.auth'])
   end
 end
