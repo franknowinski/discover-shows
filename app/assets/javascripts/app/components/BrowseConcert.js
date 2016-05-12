ae28e96bd96bc147e55bbb9bb57213fd690c5a38
frankNowinski <nowinski.frank@gmail.com>
2016-05-12 00:21:06 -0400

    WIP displaying users that are attending a concert

--- app/assets/javascripts/app/components/BrowseConcert.js
+++ app/assets/javascripts/app/components/BrowseConcert.js
@@ -6,6 +6,10 @@ var BrowseConcert = {
   controller: function(Restangular){
     var ctrl = this;
 
+    ctrl.users = this.concert.users.map(function(user){
+      return user.name || user.email;
+    });
+
     Restangular.one('artists', this.concert.artist_id).get()
       .then(function(res){
         ctrl.artist = res.artist;
