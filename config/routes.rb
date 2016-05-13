0f99a19e2943f32e264279fe3eb25517a513d408
frankNowinski <nowinski.frank@gmail.com>
2016-05-13 13:40:38 -0400

    push before installing postgres

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
   namespace :api, defaults:{format: :json} do
     namespace :v1 do
-
       resources :users, only: [:destroy] do
         resources :upcoming_concerts, only: [:index, :create, :destroy]
         resources :concerts, only: [:index]
       end
+      
       resources :artists, only: [:index, :show, :update]
       resource :users, only: [:show]
     end
